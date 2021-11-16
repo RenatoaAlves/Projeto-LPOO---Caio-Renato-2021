b79d25a27fba753430556ad808f6ffa0d3b92ecf
CaiocSiqueira <92593488+CaiocSiqueira@users.noreply.github.com>
2021-11-16 17:11:07 -0300

    Add files via upload
    Parte 09 e 10

---
+++ Celula.java
@@ -0,0 +1,27 @@
+/*
+ * Essa é a parte 9 e 10 
+ * do projeto
+ * 
+ */
+public class Celula {
+	
+	private boolean minada;
+	
+	
+	//editar depois
+	public void temMina() {
+		minada = true;
+	}
+	//editar depois
+	public void naoTemMina() {
+		minada = false;
+	}
+	
+	public boolean estadoCelula() {
+		return minada;
+	}
+	
+	
+	
+	
+}
