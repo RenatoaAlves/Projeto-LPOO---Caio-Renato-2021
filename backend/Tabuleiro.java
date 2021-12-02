04c2bd7c9049004c98fd0374fc9ea64cb53fd169
CaiocSiqueira <92593488+CaiocSiqueira@users.noreply.github.com>
2021-12-02 13:51:26 -0300

    Update Tabuleiro.java
    02/12/21

--- backend/Tabuleiro.java
+++ backend/Tabuleiro.java
@@ -1,77 +1,85 @@
 package backend;
 import java.util.Random;
+import java.util.ArrayList;
 
-public class Tabuleiro{
-	
-	
-	private Celula mapa[][] = new Celula[10][10];
+public class Tabuleiro implements AcoesTabuleiro{
 
+
+
+	private ArrayList<Celula> mapa = new ArrayList<>();
+	private ArrayList<Integer> posicaoBombas = new ArrayList<>();
+	private int numBombas = 7;
+	//private int bandeiras = numBombas;
+	private int tamMapa = 99;
+	private int tamLinha = 10;
+	
+
+		
 	Random aleatorio = new Random();
 	
 	
-	
-	//preenche o mapa
-	public void setValor(){
-		for (int i = 0; i < 10; i++) {
-			for(int j = 0; j<10; j++) {
-				 if(aleatorio.nextInt(100) %6==0) {
-		            	mapa[i][j].setMinada(true);
-		            }
-		            else {
-		            	mapa[i][j].setMinada(false);
-		        
-		            }
+	//preencher o mapa
+	public void criarCampo() {
+		
+		//gerar numeros aleatorios sem repeticao
+		while(posicaoBombas.size()<numBombas) {
+			int index = aleatorio.nextInt();
+			if(!posicaoBombas.contains(index)) {
+				posicaoBombas.add(index);
 			}
-			
-        }
-	
-		
-	}
-	
-	
-	
-	//testar se há bombas por perto
-	public int temBomba() {
-		
-		int contador = 0;
-		
-		for (int i = 1; i < 9; i++) {
-			for(int j = 1; j<9; j++) {
-			
-				
-				if(mapa[i-1][j-1].getMinada() == true){
-					contador++;
-				}
-				if(mapa[i+1][j+1].getMinada() == true) {
-					contador++;
-				}
-				if(mapa[i][j-1].getMinada() == true) {
-					contador++;
-				}
-				if(mapa[i][j+1].getMinada() == true) {
-					contador++;
-				}
-				if(mapa[i-1][j+1].getMinada() == true) {
-					contador++;
-				}
-				if(mapa[i+1][j-1].getMinada() == true) {
-					contador++;
-				}
-				if(mapa[i+1][j].getMinada() == true) {
-					contador++;
-				}
-				if(mapa[i-1][j].getMinada() == true) {
-					contador++;
-				}	
-				
-				
+
+		}
+		for(int i = 0; i<tamMapa;i++) {
+			if(posicaoBombas.contains(i)) {
+				mapa.add(new Bomba());
 			}
-			
-        }
+			else if(i%tamLinha == 0){
+				if(posicaoBombas.contains(i-tamLinha+1)||
+					posicaoBombas.contains(i-tamLinha)||
+					posicaoBombas.contains(i+tamLinha)||
+					posicaoBombas.contains(i+tamLinha+1)||
+					posicaoBombas.contains(i+1)){
+					
+					mapa.add(new CelulaAlerta());
+				}
+			}
+			else if(i%tamLinha==9){
+				if(posicaoBombas.contains(i-tamLinha-1) ||
+					posicaoBombas.contains(i-tamLinha)||
+					posicaoBombas.contains(i+tamLinha)||
+					posicaoBombas.contains(i+tamLinha-1)||
+					posicaoBombas.contains(i-1)) {
+					
+					mapa.add(new CelulaAlerta());
+				}
+			}
+			else {
+				if(posicaoBombas.contains(i-tamLinha-1) ||
+					posicaoBombas.contains(i-tamLinha+1)||
+					posicaoBombas.contains(i-tamLinha)||
+					posicaoBombas.contains(i+tamLinha)||
+					posicaoBombas.contains(i+tamLinha-1)||
+					posicaoBombas.contains(i+tamLinha+1)||
+					posicaoBombas.contains(i+1)||
+					posicaoBombas.contains(i-1)) {
+					
+					mapa.add(new CelulaAlerta());
+				}
+				else {
+					mapa.add(new CelulaVazia());
+				}
+			}
+		}
 		
-		return contador;
-			
+}
+	public void colocarBandeira(int posicao) {
+		mapa.get(posicao).setBandeira(true);
 	}
+
+
+		
+
+	
 
 	
 
