635086b34e005d6e8e03f5025f18b09c34c1fc20
CaiocSiqueira <92593488+CaiocSiqueira@users.noreply.github.com>
2021-12-09 13:42:31 -0300

    Update and rename CelulaAlerta to CelulaAlerta.java

--- backend/CelulaAlerta.java
+++ backend/CelulaAlerta.java
@@ -1,17 +1,22 @@
+package backend;
+
 public class CelulaAlerta extends Celula {
 	
 	
 	private int numBombasVizinhas;
 	
-	public CelulaAlerta() {
+	public CelulaAlerta(int posicao) {
+		super(posicao);
 		setAlerta(true);
+		setTipo(2);
 	}
 	
-	public CelulaAlerta(int numBombasVizinhas) {
+	public CelulaAlerta(int posicao, int numBombasVizinhas) {
+		super(posicao);
 		this.numBombasVizinhas = numBombasVizinhas;
 	}
 	
-	
+
 	public int getNumBombasVizinhas() {
 		return numBombasVizinhas;
 	}
