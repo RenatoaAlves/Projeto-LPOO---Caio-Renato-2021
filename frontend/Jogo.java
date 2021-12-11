baecfdd6ca82771f46703d0be42bd721141e6c16
CaiocSiqueira <92593488+CaiocSiqueira@users.noreply.github.com>
2021-12-11 18:56:22 -0300

    Add files via upload

---
+++ frontend/Jogo.java
@@ -0,0 +1,13 @@
+package frontend;
+
+public class Jogo {
+	
+	public Jogo() {
+		new Tela();
+	}
+	
+	public static void main(String[] args) {
+		new Jogo();
+	}
+
+}
---
+++ frontend/Tela.java
@@ -0,0 +1,49 @@
+package frontend;
+
+import backend.*;
+import javax.swing.*;
+import java.awt.*;
+
+public class Tela {
+	
+	private static JFrame frame;
+	
+	
+	public Tela() {
+		JPanel tela = new Tabuleiro(new GridLayout(10, 10));
+		frame = new JFrame();
+		frame.setTitle("Campo Minado " + " -  Bandeiras restantes: ");
+		frame.setVisible(true);
+		frame.setSize(550, 550);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
+		
+		
+		
+		
+		frame.setContentPane(tela);
+		frame.pack(); 
+		
+		}
+	
+	public Tela(int i) {
+		frame = new JFrame();
+		
+		frame.setTitle("Campo Minado");
+		frame.setVisible(true);
+		frame.setSize(550, 550);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
+		
+		JPanel tela = new TabuleiroMaluco(new GridLayout(10, 10));
+		
+		
+		frame.setContentPane(tela);
+		frame.pack();
+	}
+	
+	
+	
+}
