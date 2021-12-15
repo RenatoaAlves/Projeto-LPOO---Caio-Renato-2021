0c5331aee9a5c5209ef85689533d182ca2f04c0d
CaiocSiqueira <92593488+CaiocSiqueira@users.noreply.github.com>
2021-12-14 22:21:17 -0300

    Update CelulaAlerta.java

--- backend/CelulaAlerta.java
+++ backend/CelulaAlerta.java
@@ -8,17 +8,30 @@ public class CelulaAlerta extends Celula {
 	public CelulaAlerta(int posicao) {
 		super(posicao);
 		setAlerta(true);
-		setTipo(2);
 	}
 	
 	public CelulaAlerta(int posicao, int numBombasVizinhas) {
 		super(posicao);
 		this.numBombasVizinhas = numBombasVizinhas;
+		setTipo(2);
 	}
+	
+	public CelulaAlerta(int posicao, int numBombasVizinhas, boolean aberta) {
+		super(posicao);
+		this.numBombasVizinhas = numBombasVizinhas;
+		setTipo(2);
+		setDescoberta(aberta);
+	}
+	
+	
 	
 
 	public int getNumBombasVizinhas() {
 		return numBombasVizinhas;
+	}
+	
+	public void setNumBombasVizinhas(int numBombasVizinhas) {
+		this.numBombasVizinhas = numBombasVizinhas;
 	}
 	
 
