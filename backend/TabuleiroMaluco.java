fc48b22719bbe903a26af2123babd19e1d9d5e90
CaiocSiqueira <92593488+CaiocSiqueira@users.noreply.github.com>
2021-12-16 12:41:23 -0300

    Update TabuleiroMaluco.java

--- backend/TabuleiroMaluco.java
+++ backend/TabuleiroMaluco.java
@@ -9,34 +9,26 @@ import javax.swing.*;
 import java.net.URL;
 
 public class TabuleiroMaluco extends JPanel implements AcoesTabuleiro {
-	
-	
-	/**
-	 * partes do swing sairao na versao final 
-	 * e vao para as classes do front end
-	 * 
-	 */
 
+	
 	public static ArrayList<Celula> mapa = new ArrayList<>();
+	public static ArrayList<Integer> minasMarcadas = new ArrayList<>();
 	private ArrayList<Integer> posicaoBombas = new ArrayList<>();
 	private ArrayList<Integer> adjacentes = new ArrayList<>();
 	private int numBombas = 10;
 	public static int bandeiras = 10;
 	private int tamMapa = 100;
 	private int tamLinha = 10;
-	public static ArrayList<Integer> minasMarcadas = new ArrayList<>();
 	public static int bombasNoCampo = 10;
-	
-	//retirar depois(apenas testes, saira na versao final)
-	
+	private int totalAbertas = 1;
+	public static int numBombasMalucas = 0;
+
+	// retirar depois(apenas testes, saira na versao final)
 
 	Icon bomba = new ImageIcon("src\\texture_pack\\tnt.png");
 	Icon terra = new ImageIcon("src\\texture_pack\\terra.jpeg");
 	Icon grama = new ImageIcon("src\\texture_pack\\grama.jpeg");
 	Icon bandeira = new ImageIcon("src\\texture_pack\\tocha_bandeira.png");
-	
-	
-	
 
 	public TabuleiroMaluco() {
 
@@ -64,17 +56,17 @@ public class TabuleiroMaluco extends JPanel implements AcoesTabuleiro {
 
 		for (int i = 0; i < tamMapa; i++) {
 			if (posicaoBombas.contains(i)) {
-				mapa.add(new CelulaMaluca(i, true));
+				mapa.add(new CelulaMaluca(i, 3));
 				mapa.get(i).setIcon(grama);
 			} else if (i % tamLinha == 0) {
 				if (posicaoBombas.contains(i - tamLinha + 1) || posicaoBombas.contains(i - tamLinha)
 						|| posicaoBombas.contains(i + tamLinha) || posicaoBombas.contains(i + tamLinha + 1)
 						|| posicaoBombas.contains(i + 1)) {
 
-					mapa.add(new CelulaAlerta(i, contarMinas(i)));
+					mapa.add(new AlertaMaluca(i, 2, contarMinas(i)));
 					mapa.get(i).setIcon(grama);
 				} else {
-					mapa.add(new CelulaVazia(i));
+					mapa.add(new CelulaMaluca(i, 1));
 					mapa.get(i).setIcon(grama);
 				}
 			} else if (i % tamLinha == 9) {
@@ -82,10 +74,10 @@ public class TabuleiroMaluco extends JPanel implements AcoesTabuleiro {
 						|| posicaoBombas.contains(i + tamLinha) || posicaoBombas.contains(i + tamLinha - 1)
 						|| posicaoBombas.contains(i - 1)) {
 
-					mapa.add(new CelulaAlerta(i, contarMinas(i)));
+					mapa.add(new AlertaMaluca(i, 2, contarMinas(i)));
 					mapa.get(i).setIcon(grama);
 				} else {
-					mapa.add(new CelulaVazia(i));
+					mapa.add(new CelulaMaluca(i, 1));
 					mapa.get(i).setIcon(grama);
 				}
 			} else {
@@ -94,12 +86,12 @@ public class TabuleiroMaluco extends JPanel implements AcoesTabuleiro {
 						|| posicaoBombas.contains(i + tamLinha - 1) || posicaoBombas.contains(i + tamLinha + 1)
 						|| posicaoBombas.contains(i + 1) || posicaoBombas.contains(i - 1)) {
 
-					mapa.add(new CelulaAlerta(i, contarMinas(i)));
+					mapa.add(new AlertaMaluca(i, 2, contarMinas(i)));
 					mapa.get(i).setIcon(grama);
 				} else {
-					mapa.add(new CelulaVazia(i));
+					mapa.add(new CelulaMaluca(i, 1));
 					mapa.get(i).setIcon(grama);
-					
+
 				}
 			}
 		}
@@ -112,36 +104,49 @@ public class TabuleiroMaluco extends JPanel implements AcoesTabuleiro {
 
 	// clicar na mina com o botao esquerdo(abrir)
 	public void clicar(Celula celula) {
+				
+		
+		totalAbertas = 0;
+		for (int i = 0; i < mapa.size(); i++) {
+			if (mapa.get(i).getTipo() != 3) {
+				if (mapa.get(i).getDescoberta()) {
+					totalAbertas++;
+				}
+			}
+		}
+		System.out.println(totalAbertas);
+		System.out.println(minasMarcadas);
 
+		if (minasMarcadas.size() == bombasNoCampo && (totalAbertas + numBombas == mapa.size())) {
+			JOptionPane.showMessageDialog(null, "Parabéns!", "Não fez mais que sua obrigação",
+					JOptionPane.DEFAULT_OPTION);
+			System.exit(1);
+		}
 
 		if (!celula.getBandeira()) {
 			celula.setDescoberta(true);
 			celula.setBackground(Color.white);
 
 			if (celula.getTipo() == 3) {
-				
-				if(!celula.getMinada()) {
-					celula.setIcon(terra);
-				}
-				else {
-					celula.setExplosao();
-					celula.setIcon(bomba);
-					JOptionPane.showMessageDialog(null, "YOU ARE A FAILURE","U ARE A WASTE OF CARBON", JOptionPane.INFORMATION_MESSAGE);
-					System.exit(1);
-					
-				}
-				
+				celula.setExplosao();
+				celula.setIcon(bomba);
+				JOptionPane.showMessageDialog(null, "YOU ARE A FAILURE", "U ARE A WASTE OF CARBON",
+						JOptionPane.INFORMATION_MESSAGE);
+				System.exit(1);
+
 			}
 
 			if (celula.getTipo() == 2) {
+
 				int quantidade = celula.getNumBombasVizinhas();
 				celula.setText(String.valueOf(quantidade));
 				celula.setHorizontalTextPosition(SwingConstants.CENTER);
-		        celula.setForeground(Color.white);
+				celula.setForeground(Color.white);
 				celula.setIcon(terra);
 			}
 
 			if (celula.getTipo() == 1) {
+
 				int posicao = celula.getPosicao();
 				celula.setIcon(terra);
 
@@ -215,65 +220,92 @@ public class TabuleiroMaluco extends JPanel implements AcoesTabuleiro {
 				}
 
 			}
-						
-			
-			for(int i=0;i<adjacentes.size();i++) {
-			
+
+			for (int i = 0; i < adjacentes.size(); i++) {
+
 				if (mapa.get(adjacentes.get(i)).getTipo() == 2) {
+
 					int quantidade = mapa.get(adjacentes.get(i)).getNumBombasVizinhas();
 					mapa.get(adjacentes.get(i)).setText(String.valueOf(quantidade));
 					mapa.get(adjacentes.get(i)).setForeground(Color.white);
 					mapa.get(adjacentes.get(i)).setHorizontalTextPosition(SwingConstants.CENTER);
 					mapa.get(adjacentes.get(i)).setIcon(terra);
 					mapa.get(adjacentes.get(i)).setDescoberta(true);
-					
 
 				}
-				
-				if((!mapa.get(adjacentes.get(i)).getBandeira())) {
-					if(!mapa.get(adjacentes.get(i)).getDescoberta()) {
 
-						clicar(mapa.get(adjacentes.get(i)));
+				if ((!mapa.get(adjacentes.get(i)).getBandeira())) {
+					if (!mapa.get(adjacentes.get(i)).getDescoberta()) {
+						
+						if(mapa.get(adjacentes.get(i)).getTipo() != 3) {
+							clicar(mapa.get(adjacentes.get(i)));
+						}
+						
 					}
 
 				}
 			}
+			totalAbertas = 0;
+			for (int i = 0; i < mapa.size(); i++) {
+				if (mapa.get(i).getTipo() != 3) {
+					if (mapa.get(i).getDescoberta()) {
+						totalAbertas++;
+					}
+				}
+			}
+
+			if (minasMarcadas.size() == bombasNoCampo && (totalAbertas + numBombas == mapa.size())) {
+				JOptionPane.showMessageDialog(null, "Parabéns!", "Não fez mais que sua obrigação",
+						JOptionPane.DEFAULT_OPTION);
+				System.exit(1);
+			}
 
 		}
 	}
-	
-	//clicar com botao direito(bandeira)
+
+	// clicar com botao direito(bandeira)
 	public void colocarBandeira(Celula celula) {
+		
 		ativarMaluca(celula);
+		
 
-		if((!celula.getBandeira())&&(!celula.getDescoberta())&&bandeiras>0) {
-			
-			celula.setBandeira(true);
-			celula.setIcon(bandeira);
-			
-			bandeiras--;
-
-		}
-		else if(celula.getBandeira()) {
-			
+		if (celula.getBandeira()) {
 			celula.setBandeira(false);
 			celula.setIcon(grama);
 			bandeiras++;
+
 		}
-		
-		for(int i=0; i<mapa.size();i++) {
-			if(mapa.get(i).getTipo()==3 && mapa.get(i).getBandeira()) {
-				if(!minasMarcadas.contains(mapa.get(i).getPosicao())) {
+
+		else if ((!celula.getBandeira()) && (!celula.getDescoberta()) && bandeiras > 0) {
+			celula.setIcon(bandeira);
+			celula.setBandeira(true);
+			bandeiras--;
+		}
+
+		for (int i = 0; i < mapa.size(); i++) {
+			if (mapa.get(i).getTipo() == 3 && mapa.get(i).getBandeira()) {
+				if (!minasMarcadas.contains(mapa.get(i).getPosicao())) {
 					minasMarcadas.add(mapa.get(i).getPosicao());
+				}
+
+			}
+
+		}
+		totalAbertas = 0;
+		for (int i = 0; i < mapa.size(); i++) {
+			if (mapa.get(i).getTipo() != 3) {
+				if (mapa.get(i).getDescoberta()) {
+					totalAbertas++;
 				}
 			}
 		}
-				
-		if(minasMarcadas.size() == bombasNoCampo) {
-			JOptionPane.showMessageDialog(null, "Parabéns!","Não fez mais que sua obrigação", JOptionPane.DEFAULT_OPTION);
+
+		if (minasMarcadas.size() == bombasNoCampo && (totalAbertas + numBombas == mapa.size())) {
+			JOptionPane.showMessageDialog(null, "Parabéns!", "Não fez mais que sua obrigação",
+			JOptionPane.DEFAULT_OPTION);
 			System.exit(1);
 		}
-		
+
 	}
 
 	public int contarMinas(int i) {
@@ -325,7 +357,6 @@ public class TabuleiroMaluco extends JPanel implements AcoesTabuleiro {
 	}
 
 	public void adicionarCelulas() {
-	
 		for (int i = 0; i < mapa.size(); i++) {
 			add(mapa.get(i));
 			mapa.get(i).setPreferredSize(new Dimension(50, 50));
@@ -333,22 +364,20 @@ public class TabuleiroMaluco extends JPanel implements AcoesTabuleiro {
 	}
 
 	public void ativarMaluca(Celula celula) {
-		
-		
+
 		ArrayList<Integer> updateAlertas = new ArrayList<>();
-		
-		
+
 		int probabilidade = aleatorio.nextInt(10);
-		
-		if(celula.getTipo() == 3 && (probabilidade == 2 || probabilidade == 7)) {
+
+		if (celula.getTipo() == 3/* && (probabilidade == 2 || probabilidade == 7)*/) {
+			minasMarcadas.add(celula.getPosicao());
 			mapa.get(celula.getPosicao()).setMinada(false);
 			mapa.get(celula.getPosicao()).setTipo(1);
-			bombasNoCampo--;
-			
+
 		}
-		
+
 		int posicao = celula.getPosicao();
-		
+
 		if (posicao < tamLinha) {
 			if (posicao == 0) {
 				updateAlertas.add(posicao + tamLinha);
@@ -417,37 +446,28 @@ public class TabuleiroMaluco extends JPanel implements AcoesTabuleiro {
 			updateAlertas.add(posicao + tamLinha - 1);
 
 		}
-	
-		
-		for(int i = 0; i<updateAlertas.size();i++) {
-			if(mapa.get(updateAlertas.get(i)).getTipo() == 2) {
-				
-				if(mapa.get(updateAlertas.get(i)).getNumBombasVizinhas() == 1) {
-					mapa.set(updateAlertas.get(i), new CelulaVazia(celula.getPosicao()));
-					if(mapa.get(updateAlertas.get(i)).getDescoberta()) {
+
+		for (int i = 0; i < updateAlertas.size(); i++) {
+			if (mapa.get(updateAlertas.get(i)).getTipo() == 2) {
+
+				if (mapa.get(updateAlertas.get(i)).getNumBombasVizinhas() == 1) {
+					mapa.set(updateAlertas.get(i), new CelulaMaluca(celula.getPosicao(), 1));
+					if (mapa.get(updateAlertas.get(i)).getDescoberta()) {
 						mapa.get(updateAlertas.get(i)).setIcon(terra);
-					}
-					else {
+		
+					} else {
 						mapa.get(updateAlertas.get(i)).setIcon(grama);
 					}
-				}
-				else if(mapa.get(updateAlertas.get(i)).getNumBombasVizinhas() > 1) {
+				} else if (mapa.get(updateAlertas.get(i)).getNumBombasVizinhas() > 1) {
 					int numAtual = mapa.get(updateAlertas.get(i)).getNumBombasVizinhas();
-					((CelulaAlerta)mapa.get(updateAlertas.get(i))).setNumBombasVizinhas(numAtual-1);
-				}
-				
-			}
-			
-			
-		}
-		
-		
-		
+					((AlertaMaluca) mapa.get(updateAlertas.get(i))).setNumBombasVizinhas(numAtual - 1);
 
-				
-		
+				}
+
+			}
+
+		}
+
 	}
 
-
 }
-
