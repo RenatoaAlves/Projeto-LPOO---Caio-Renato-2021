7b688dc2130503999c5a3c20875858ca81c29c24
CaiocSiqueira <92593488+CaiocSiqueira@users.noreply.github.com>
2021-12-17 20:31:30 -0300

    Update Ranking.java

--- backend/Ranking.java
+++ backend/Ranking.java
@@ -1,39 +1,44 @@
 package backend;
 
 import java.util.*;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
+import java.io.File;
 import java.io.IOException;
+import java.io.FileNotFoundException;
 
-public class Ranking {
-	
-	private String nome;
-	private int pontos;	
-	
-	ArrayList<Player> listaRanking = new ArrayList<>();
-	
-	public Ranking(String nome, int pontos) {
-		this.nome = nome;
-		this.pontos = pontos;
-	}
-	
-	public void adicionarRank() {
-		Player jogador = new Player(nome, pontos);	
+public class Ranking{
 		
-		try {
-			BufferedWriter writer = new BufferedWriter(new FileWriter("ranking.txt"));
-			writer.write(jogador.getNome() + "," + jogador.getPontos());
-			writer.close();
-		} catch (IOException e) {
-			
-			e.printStackTrace();
+	
+	private ArrayList<Player> listaRanking = new ArrayList<>();
+	public static ArrayList<String> rankingString = new ArrayList<>();
+	
+	public void lerRanking() throws FileNotFoundException {
+		
+		File file = new File("ranking.txt");
+		Scanner scan = new Scanner(file);
+		
+		while(scan.hasNextLine()) {
+			String dados = scan.nextLine();
+			String[] info = dados.split(" ");
+			listaRanking.add(new Player(info[0], Integer.parseInt(info[1])));
 		}
+		scan.close();
 		
+		Collections.sort(listaRanking);
+		
+		for(int i = 0; i<listaRanking.size(); i++) {
+			rankingString.add(listaRanking.get(i).getNome() + " " + listaRanking.get(i).getTempo());
+		}
 	}
 	
-	public void setPontos(int pontos) {
-		this.pontos = pontos;
+	public ArrayList<String> getRankingString(){
+		
+		return rankingString;
 	}
+	
+	
+	
+
+	
 	
 	
 	
